d222e246abadc168e0ad44246c27d7c7984f73d7
Daniel James <dwhjames@gmail.com>
2014-08-17 19:50:00 -0400

    exclude clojure 1.5.1 transitive dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.4815.12"]
+                 [com.datomic/datomic-free "0.9.4815.12"
+                  :exclusions [org.clojure/clojure]]
                  [org.clojure/test.check "0.5.9" :scope "test"]]
   :plugins [[lein-marginalia "0.7.1"]]
   :marginalia {:javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"]})
